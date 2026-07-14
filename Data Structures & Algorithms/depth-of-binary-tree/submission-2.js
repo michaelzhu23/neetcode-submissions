a30564de90bb57fb2d53418d3dd5de4e7184de38
Michael Zhu <mzhu23@gmail.com>
2026-07-13 19:57:00 -0700

    Add: depth-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-2.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxDepth(root) {
+        const queue = [];
+        if(root) queue.push(root)
+
+        let level = 0;
+
+        while(queue.length > 0){
+            const size = queue.length;
+
+            for(let i = 0; i < size; i++){
+                const node = queue.shift();
+
+                if(node.left) queue.push(node.left);
+                if(node.right) queue.push(node.right);
+            }
+
+            level++;
+        }
+        return level;
+    }
+}
