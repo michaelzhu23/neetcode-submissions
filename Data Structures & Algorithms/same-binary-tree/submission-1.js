1d201644d59076899ddf59323f3b0d8c98babaa0
Michael Zhu <mzhu23@gmail.com>
2026-07-15 22:43:19 -0700

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p, q) {
+        const queue = [[p, q]];
+
+        while (queue.length > 0) {
+            const [nodeP, nodeQ] = queue.pop();
+
+            if (nodeP === null && nodeQ === null) continue;
+            if (nodeP === null || nodeQ === null || nodeP.val !== nodeQ.val) {
+                return false;
+            }
+
+            queue.push([nodeP.left, nodeQ.left]);
+            queue.push([nodeP.right, nodeQ.right]);
+        }
+
+        return true;
+    }
+}
